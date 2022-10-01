1b619cd2dc5e4a2aed743ceb72628c78cd1918b9
shakthi <shakthikumar13456789@gmail.com>
2022-10-01 20:30:28 +0530

    Create header

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const nav = document.querySelector("header nav");
+const menu = document.querySelector("header .menu");
+menu.children[1].style.display = "none";
+
+menu.addEventListener("click", (e) => {
+  nav.classList.toggle("show");
+
+  if (nav.classList.contains("show")) {
+    menu.children[0].style.display = "none";
+    menu.children[1].style.display = "block";
+    document.querySelector("body").style.height = "100vh";
+    document.querySelector("body").style.overflow = "hidden";
+  } else {
+    menu.children[1].style.display = "none";
+    menu.children[0].style.display = "block";
+    document.querySelector("body").style.height = "100%";
+    document.querySelector("body").style.overflow = "auto";
+  }
+});
